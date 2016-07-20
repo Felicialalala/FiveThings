bc590588bf0fd7116670a4d76502d1c2042b612e
Felicialalala <mailscometoyyt@126.com>
2016-07-20 11:51:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/fivethings/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.fivethings;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton1();
+        addListenerOnButton2();
+    }
+
+    public void addListenerOnButton1(){
+        final Context context = this;
+        button1 = (Button)findViewById(R.id.dresses);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, dresses.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void addListenerOnButton2(){
+        final Context context = this;
+        button2 = (Button)findViewById(R.id.tops);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,dresses.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
